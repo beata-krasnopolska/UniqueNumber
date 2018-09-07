401281864af0738c8d9408961d116ef739353f59
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-09-07 20:13:57 +0200

    Add project files.

---
+++ UniqueNumber/Finder.cs
@@ -0,0 +1,24 @@
+﻿
+namespace UniqueNumber
+{
+    class Finder
+    {
+        public int singleNumber(int[] nums)
+        {
+            int once = 0; int twice = 0; int threes; int x = 0;
+            int i = 0;
+            foreach (int num in nums)
+            {               
+                    x = nums[i];
+                    twice |= once & x; //add it to twice if it exists in once
+                    once ^= x; //it exists in once, remove it, otherwise, add it
+                    threes = once & twice;  //if x is in once and twice, add it to Threes.
+                                            // and thus 
+                    once &= ~threes; //remove x from once
+                    twice &= ~threes; //remove x from twice
+                i++;              
+            }        
+            return once;
+        }
+    }
+}
---
+++ UniqueNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UniqueNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[3] { 2, 2, 1 };
+
+            var unique = new Finder();
+            var uniqueNumber = unique.singleNumber(nums);
+
+            Console.WriteLine(uniqueNumber);
+            Console.Read();
+        }
+    }
+}
